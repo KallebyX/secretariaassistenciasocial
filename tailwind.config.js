473bd337e1944cd4111340ad5aa6999255931654
Kalleby Evangelho <kallebyevangelho03@gmail.com>
2025-08-19 17:39:13 +0000

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,36 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./{App,types,components,pages,context}/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        'prefeitura-amarelo': 'rgb(245, 240, 80)',
-        'prefeitura-verde': 'rgb(83, 168, 85)',
-        'prefeitura-vermelho': 'rgb(218, 57, 50)',
-      }
+        brand: {
+          primary: '#2E7D32', // Verde mais escuro e sóbrio
+          secondary: '#FFC107', // Amarelo vibrante
+          danger: '#D32F2F', // Vermelho para alertas
+        },
+        neutral: {
+          '50': '#f8fafc',
+          '100': '#f1f5f9',
+          '200': '#e2e8f0',
+          '300': '#cbd5e1',
+          '400': '#94a3b8',
+          '500': '#64748b',
+          '600': '#475569',
+          '700': '#334155',
+          '800': '#1e293b',
+          '900': '#0f172a',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
     },
   },
   plugins: [],
-}
+};
